37869231dadc5524cac95f2650e18b6ad266f7df
Andrey Fedorov <dron81998@yandex.ru>
2023-01-13 09:18:24 +0300

    Исправлен код DAO и сущностей

--- src/main/java/ru/hh/school/entity/Resume.java
+++ src/main/java/ru/hh/school/entity/Resume.java
@@ -1,9 +1,11 @@
 package ru.hh.school.entity;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Objects;
 
 //TODO: оформите entity
+@Entity
+@Table(name = "resume")
 public class Resume {
   // TODO: сделать так, чтобы id брался из sequence-а
   // таким образом, мы сможем отправлять в бд запросы батчами.
@@ -15,15 +17,29 @@ public class Resume {
   // https://vladmihalcea.com/from-jpa-to-hibernates-legacy-and-enhanced-identifier-generators/
 
   @Id
-  @GeneratedValue(/* здесь место для вашего кода */)
+  //@GeneratedValue(/* здесь место для вашего кода */)
+  @GeneratedValue(generator = "resumeIdSeq", strategy = GenerationType.SEQUENCE)
+  @SequenceGenerator(name = "resumeIdSeq", sequenceName = "resume_id_seq", allocationSize = 10)
   private Integer id;
-
+  @Column
   private String description;
-
+  @Deprecated
   Resume() {}
 
   public Resume(String description) {
     this.description = description;
   }
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Resume resume = (Resume) o;
+    return Objects.equals(id, resume.id) && Objects.equals(description, resume.description);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, description);
+  }
 }
