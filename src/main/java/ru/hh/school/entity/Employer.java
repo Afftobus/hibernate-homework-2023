0d0cb6728a87c699e7cf96a48fab945d4b530b5b
NaAN <a.nagatkin@mail.ru>
2023-01-14 20:27:36 +0300

    1. Оформлены сущности Area, Employer, Resume, Vacancy; 2. Написаны запросы в EmployerDao, VacancyDao; 3. save заменен на saveOrUpdate в GenericDao; 4. Cделано сохранение состояния работодателя и его вакансий в EmployerService.

--- src/main/java/ru/hh/school/entity/Employer.java
+++ src/main/java/ru/hh/school/entity/Employer.java
@@ -1,23 +1,45 @@
 package ru.hh.school.entity;
 
+import com.sun.istack.NotNull;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 //TODO: оформите entity
+@Entity
+@Table(name = "employer")
 public class Employer {
-
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "employer_id")
   private Integer id;
 
+  @Column(name = "company_name")
+  @NotNull
   private String companyName;
 
   // не используйте java.util.Date
   // https://docs.jboss.org/hibernate/orm/5.3/userguide/html_single/Hibernate_User_Guide.html#basic-datetime-java8
+  @CreationTimestamp
+  @Column(name = "creation_time", nullable = false, updatable = false)
   private LocalDateTime creationTime;
 
+  @OneToMany(mappedBy = "employer",
+      cascade = {CascadeType.ALL},
+      orphanRemoval = true)
   private List<Vacancy> vacancies = new ArrayList<>();
 
+  @Column(name = "block_time")
   private LocalDateTime blockTime;
 
   public List<Vacancy> getVacancies() {
@@ -48,17 +70,21 @@ public class Employer {
   //
   // https://vladmihalcea.com/hibernate-facts-equals-and-hashcode/
   // https://docs.jboss.org/hibernate/orm/5.3/userguide/html_single/Hibernate_User_Guide.html#mapping-model-pojo-equalshashcode
+
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
+
     Employer employer = (Employer) o;
-    return Objects.equals(companyName, employer.companyName);
+
+    return id.equals(employer.getId()) && companyName.equals(employer.getCompanyName());
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(companyName);
+    int result = id != null ? id.hashCode() : 0;
+    result = 31 * result + (companyName != null ? companyName.hashCode() : 0);
+    return result;
   }
-
 }
--- src/main/java/ru/hh/school/entity/Vacancy.java
+++ src/main/java/ru/hh/school/entity/Vacancy.java
@@ -10,29 +10,43 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 //TODO: оформите entity
+@Entity
+@Table(name = "vacancy")
 public class Vacancy {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "vacancy_id")
   private Integer id;
 
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "employer_id")
   private Employer employer;
-
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "area_id")
   private Area area;
 
+  @Column(name = "title")
   private String title;
 
+  @Column(name = "description")
   private String description;
 
+  @Column(name = "compensation_from")
   private Integer compensationFrom;
 
-  private Integer  compensationTo;
+  @Column(name = "compensation_to")
+  private Integer compensationTo;
 
+  @Column(name = "compensation_gross")
   private Boolean compensationGross;
 
+  @Column(name = "creation_time")
   private LocalDateTime creationTime;
 
+  @Column(name = "archiving_time")
   private LocalDateTime archivingTime;
 
   public Vacancy() {
@@ -94,13 +108,17 @@ public class Vacancy {
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
+
     Vacancy vacancy = (Vacancy) o;
-    return Objects.equals(id, vacancy.id);
+
+    return id.equals(vacancy.getId()) && title.equals(vacancy.getTitle());
   }
 
   @Override
   public int hashCode() {
-    return 17;
+    int result = id != null ? id.hashCode() : 0;
+    result = 31 * result + (title != null ? title.hashCode() : 0);
+    result = 31 * result + (description != null ? description.hashCode() : 0);
+    return result;
   }
-
 }
